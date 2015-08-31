8bef447f8d9a950fa00cd74625d26aa425a28bff
Alex Wiggins <alex.d.wiggins@gmail.com>
2015-08-31 11:33:54 +0100

    MoEDict minor formatting updates

--- MoEDict/MoEDict-Pleco-Conversion.sql
+++ MoEDict/MoEDict-Pleco-Conversion.sql
@@ -1,3 +1,5 @@
+-- Create new table (combined) with relevant columns for Pleco user dictionary.
+
 create table combined as
 select definitions.id, definitions.idx, entries.title, heteronyms.pinyin,
 definitions.type, definitions.def, definitions.example, definitions.quote,
@@ -5,12 +7,19 @@ definitions.synonyms, definitions.antonyms, definitions.link
 from definitions, heteronyms, entries
 where definitions.heteronym_id = heteronyms.id and heteronyms.entry_id = entries.id;
 
+-- Combine definition fields into single field.
+
+-- Process (Western) comma separators (replace with Pleco new line or Chinese list separators):
 update combined set example = replace(example, ',', '');
 update combined set quote = replace(quote, ',', '');
 update combined set synonyms = replace(synonyms, ',', '、');
 update combined set antonyms = replace(antonyms, ',', '、');
 update combined set link = replace(link, ',', '');
 
+/* Replace null entries to empty strings '' for combining/counting later
+(otherwise can causes problems with null results,
+especially when using type for counting number of definitions),
+and at same time add label for synonyms and antonyms:*/
 update combined set
 type = case when coalesce(type, '') = '' then '' else type end;
 update combined set
@@ -26,6 +35,7 @@ antonyms = case when coalesce(antonyms, '') = '' then '' else '反：'||antonyms
 update combined set
 link = case when coalesce(link, '') = '' then '' else link end;
 
+-- Now combine into single field newdef:
 alter table combined add column newdef;
 update combined set newdef = '';
 update combined set newdef = case when coalesce(def, '') = ''
@@ -46,15 +56,21 @@ update combined set newdef = (case when coalesce(link, '') = ''
 then newdef else (case when coalesce(newdef, '') = ''
 then link else newdef||''||link end) end);
 
+/* Add numbering to combined definitions:
+order by idx then count up to current id.*/
+
 create table combined2 as
 select id, title, pinyin, idx, type, newdef,
-(select count(*) from combined c2 where c1.title=c2.title and c1.pinyin=c2.pinyin and c1.type=c2.type and c2.id<=c1.id) as defid, (select count(*) from combined c2 where c1.title=c2.title and c1.pinyin=c2.pinyin and c1.type=c2.type) as defcount
+(select count(*) from combined c2 where c1.title=c2.title and c1.pinyin=c2.pinyin and c1.type=c2.type and c2.id<=c1.id) as defid,
+(select count(*) from combined c2 where c1.title=c2.title and c1.pinyin=c2.pinyin and c1.type=c2.type) as defcount
 from combined c1;
 
+-- Add numbering (only if more than one definition in unique title, pinyin, type):
 update combined2 set newdef =
 (case when defcount<=1 then newdef
 else '(@'||defid||'@) '||newdef end);
 
+-- Replace with circled numbers:
 update combined2 set newdef = replace(newdef, '(@1@)','①');
 update combined2 set newdef = replace(newdef, '(@2@)','②');
 update combined2 set newdef = replace(newdef, '(@3@)','③');
@@ -75,7 +91,9 @@ update combined2 set newdef = replace(newdef, '(@17@)','⑰');
 update combined2 set newdef = replace(newdef, '(@18@)','⑱');
 update combined2 set newdef = replace(newdef, '(@19@)','⑲');
 update combined2 set newdef = replace(newdef, '(@20@)','⑳');
+-- Note: max(defcount) suggests only need to go up to 19.
 
+-- Group by part of speech (title, pinyin, type):
 create table combined3 as
 select title, pinyin, idx, type, defcount,
 group_concat(newdef, '') as newdef3
@@ -84,18 +102,48 @@ from combined2
 order by title, pinyin, defid)
 group by title, pinyin, type;
 
-update combined3 set newdef3 =
-(case when coalesce(type, '') = ''
-then '◆ '||newdef3 else
-(case when defcount>1
-then '◆ <'||type||'> '||newdef3
-else '◆ <'||type||'> '||newdef3 end) end);
+-- Add part of speech (type) count (for determining use of bullet characters):
+create table combined3a as
+select title, pinyin, idx, type, defcount, newdef3,
+(select count(*) from combined3 c2 where c1.title=c2.title and c1.pinyin=c2.pinyin) as partcount
+from combined3 c1;
 
+-- Add bullet character if more than one part of speech (type):
+alter table combined3a add column bullet;
+update combined3a set bullet =
+(case when partcount>1
+then '◆ '
+else '' end);
+
+-- Add part of speech (type), bullets and new lines as appropriate:
+update combined3a set newdef3 =
+(case when coalesce(type, '') = ''
+then bullet||newdef3 else
+(case when defcount>1
+then bullet||'<'||type||'> '||newdef3
+else bullet||'<'||type||'> '||newdef3 end) end);
+
+-- Group by Hanzi/Pinyin (title, pinyin):
 create table combined4 as
 select title, pinyin, group_concat(newdef3, '') as newdef4
 from (select title, pinyin, idx, type, newdef3
-from combined3
+from combined3a
 order by title, pinyin, idx)
 group by title, pinyin;
 
-select count(*) from combined4;
+-- Replace Chinese brackets with Western brackets in pinyin field (to show in Pleco):
+update combined4 set pinyin=
+replace(pinyin, '（', ' (');
+update combined4 set pinyin=
+replace(pinyin, '）', ') ');
+
+-- Move bracketed Western names from title (Hanzi) field to start of definition field:
+update combined4 set
+newdef4=substr(title,instr(title,'('))||' '||newdef4,
+title=substr(title,1,instr(title,'(')-1)
+where title like '%)';
+
+-- Output counts for consistency checking:
+select count(*) from combined2; -- (unique title, pinyin, type, def) = 213486;
+select count(*) from combined3; -- (unique title, pinyin, type) = 171378;
+select count(*) from combined4; -- (unique title, pinyin) = 165810.
