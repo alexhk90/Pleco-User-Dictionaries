9fdd42efecce4be404239b27aef15ae966fa8e00
Alex Wiggins <alex.d.wiggins@gmail.com>
2015-09-02 23:45:10 +0100

    MoE-Minnan updated Python script (v02)

--- MoE-Minnan/MoE-Minnan-Pleco-Conversion.py
+++ MoE-Minnan/MoE-Minnan-Pleco-Conversion.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
-print( "2015-09-02 MoE-Minnan-Pleco-Conversion Script" )
+print("2015-09-02 MoE-Minnan-Pleco-Conversion Script")
 # See "MoE-Minnan-Pleco-Conversion.txt" for associated notes
 
 # use Python json module
 import json
 
 DataFile = "dict-twblg.json"
-OutputFile = "MoE-Minnan-flashcards-v01.txt"
+OutputFile = "MoE-Minnan-flashcards-v02.txt"
 
 PLECO_NEW_LINE = ""
+DEF_NUMBERS = ['①','②','③','④','⑤','⑥','⑦','⑧','⑨','⑩','⑪','⑫','⑬','⑭','⑮','⑮','⑰','⑱','⑲','⑳']
 
-print( "Input JSON data file:", DataFile )
-print( "Output Pleco flashcards file:", OutputFile )
+print("Input JSON data file:", DataFile)
+print("Output Pleco flashcards file:", OutputFile)
 
-print( "Loading input data file..." );
-Entries = json.load( open( DataFile ) )
+print("Loading input data file...");
+Entries = json.load(open(DataFile))
 
-print( "- Number of entries in input data file = ", len( Entries ) )
+print("- Number of entries in input data file = {0}".format(len(Entries)))
 
-print( "Creating blank output file (overwrites if exists)..." )
-FileOut = open( OutputFile, "w" )
+print("Creating blank output file (overwrites if exists)...")
+FileOut = open(OutputFile, "w")
 
-print( "Looping through and processing input data file entries..." )
+print("Looping through and processing input data file entries...")
 EntriesProcessed = 0
 EntriesOutput = 0
 for Entry in Entries:
-  # parse entry and output to Pleco flashcards
+  # parse entry and output to Pleco flashcards (one per entry-heteronym)
 
   # title (Hanzi) from entry level:
   Hanzi = Entry['title'] 
@@ -36,49 +37,55 @@ for Entry in Entries:
     # trs (Pronunciation) and synonyms from heteronym level:
     Pinyin = Het['trs']
 
-    DefString = ""
+    # heteronym (output entry) level string
+    DefStrings = []
+    DefNo = 0
     HetDefs = Het['definitions']
-    for Definition in HetDefs:
+    DefCount = len(HetDefs)
+    for Definition in HetDefs:      
+      # definition level string
+      CurrentDefString = ""
+
+      # numbering
+      if DefCount > 1:
+        CurrentDefString += DEF_NUMBERS[DefNo] + " "
+        DefNo += 1
+
       # type, def, examples from definition level:
       CurrentType = Definition['type']
+      if CurrentType != "":
+        CurrentDefString += "<" + CurrentType + ">" + " "
+
       CurrentDef = Definition['def']
-      
-      CurrentDefString = "<" + CurrentType + ">" + PLECO_NEW_LINE
       CurrentDefString += CurrentDef
 
-      if 'examples' in Definition:
-        ExampleString = ""
-        CurrentExamples = Definition['examples']
-        for Example in CurrentExamples:
-          if ExampleString == "": # should be built-in function for this StrJoin
-            ExampleString = Example
-          else:
-            ExampleString += PLECO_NEW_LINE + Example
+      if 'example' in Definition:
+        CurrentExamples = Definition['example']
+        ExampleSeparator = PLECO_NEW_LINE + "如："
+        ExampleString = "如：" + ExampleSeparator.join(CurrentExamples)
         CurrentDefString += PLECO_NEW_LINE + ExampleString
 
-      # add to output definition string
-      if DefString == "": # should be built-in function for this StrJoin
-        DefString = CurrentDefString
-      else: 
-        DefString += PLECO_NEW_LINE + CurrentDefString
+      DefStrings.append(CurrentDefString)
 
-    # add synonyms if element exists for heteronym
+    # collect and join output definition strings
+    DefString = PLECO_NEW_LINE.join(DefStrings)
+
     if 'synonyms' in Het: 
       Syns = Het['synonyms']
       DefString += PLECO_NEW_LINE + "似：" + Syns
 
     # Output to flashcard format...
     CardString = Hanzi + "\t" + "@" + Pinyin + "\t" + DefString + "\n"
-    FileOut.write( CardString )
+    FileOut.write(CardString)
     EntriesOutput += 1
     # Note: one Pleco entry created for every entry-heteronym from input
 
   EntriesProcessed += 1
 
-# FileOut.close( )
+# FileOut.close()
 
-print( "- Input entries processed (input data file) = ", EntriesProcessed )
-print( "- Number of resulting Pleco entries (output flashcards) = ", EntriesOutput )
+print("- Input entries processed (input data file) = {0}".format(EntriesProcessed))
+print("- Number of resulting Pleco entries (output flashcards) = {0}".format(EntriesOutput))
 
-print( "MoE-Minnan-Pleco-Conversion Script Completed." )
-exit( 0 )
+print("MoE-Minnan-Pleco-Conversion Script Completed.")
+exit(0)
